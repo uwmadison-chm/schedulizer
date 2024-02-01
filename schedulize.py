0372eb5187253d0eb69d4f86938fc965d1133741
Nicholas Vanhaute <nvanhaute@wisc.edu>
2024-02-01 16:16:04 -0600

    Flesh out class

--- schedulize.py
+++ schedulize.py
@@ -1,18 +1,25 @@
 class Time:
-    hours: int
-    minutes: int
+    def __init__(self, hours, minutes):
+        if hours not in range(0, 24):
+            raise ValueError('hours must be in the range [0..23]')
+        if minutes not in range(0, 60):
+            raise ValueError('minutes must be in the range [0..59]')
+        self._minutes = hours * 60 + minutes
 
     def __lt__(x,y):
-        return x.hours < y.hours or \
-            (x.hours == y.hours and x.minutes < y.minutes)
-    
+        return x._minutes < y._minutes
+        
     def __gt__(x,y):
-        return x.hours > y.hours or \
-            (x.hours == y.hours and x.minutes > y.minutes)
+        return x._minutes > y._minutes
     
     def __eq__(x,y):
-        return x.hours == y.hours and x.minutes == y.minutes
-
+        return x._minutes == y._minutes
+    
+    def __repr__(self):
+        return f'{self._minutes // 60}:{str(self._minutes % 60).zfill(2)}'        
 
 def schedulize(start: Time, end: Time):
-    pass
+    if not (isinstance(start, Time) and isinstance(end, Time)):
+        raise TypeError("start and end must be instances of Time")
+    
+    
